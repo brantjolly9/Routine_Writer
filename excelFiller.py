dd1c05a86b089440f66206f29ceb59a8d779e128
Brant Jolly <brant.jolly@atcss.com>
2024-11-01 12:48:33 -0400

    updated pycom

--- excelFiller.py
+++ excelFiller.py
@@ -2,20 +2,22 @@ import lxml.etree as et
 from pprint import pprint
 import re
 
+
+def split_rungs(l5xPath):
+    rungs = []
+    xmlDoc = et.parse(l5xPath)
+    root = xmlDoc.getroot()
+    RLLContent = root.findall('Controller/Programs/Program/Routines/Routine/RLLContent/Rung')
+    for rung in RLLContent:
+        rungText = rung.find("Text").text.strip()
+        pairs = re.findall(r"\((.*?)\)", rungText)
+        for pair in pairs:
+            if "," in pair:
+                rungs.append(tuple(pair.split(",")))
+            else:
+                rungs.append(("1", pair))
+    return rungs
+
 l5xPath = "exeTesting\\Exported_Files\\Configuration_Routine_RLL.L5X"
-xmlDoc = et.parse(l5xPath)
-root = xmlDoc.getroot()
-RLLContent = root.findall('Controller/Programs/Program/Routines/Routine/RLLContent/Rung')
-
-rungs = []
-
-for rung in RLLContent[:10]:
-    rungText = rung.find("Text").text.strip()
-    pairs = re.findall(r"\((.*?)\)", rungText)
-    for pair in pairs:
-        if "," in pair:
-            pair = pair.split(",")
-    rungs.append(pair)
-
-
+rungs = split_rungs(l5xPath)
 pprint(rungs)
