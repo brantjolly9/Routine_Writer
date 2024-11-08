617c781e907d224524acfb0c582512dee99d8c92
Your Name <you@example.com>
2024-11-08 13:46:09 -0500

    reworking main function

--- main.py
+++ main.py
@@ -67,9 +67,11 @@ def combo(l5xPath):
     allRungs = get_all_rungs(l5xPath)
     parsedRungs = parse_routine(allRungs)
     exl = write_param_sheet(parsedRungs, "csv_testing.csv") 
+    
     print("RAN")
 
-def main():
+
+def old_main():
     # Refer to directory structure in README.txt
     # workingDir will be located in the root directory, then the *_Files directories will be located in the working dir
     # leave testRootDir as is for this to work (will be changed later)
@@ -92,7 +94,6 @@ def main():
     excelPath, l5xFiles = check_working_files(workingDir, logger)
     exportedFilesPath = verifiedPaths[exportedFilesFolder] 
     resultFilesPath = verifiedPaths[resultFilesFolder]
-
     print("\nThe Program has created or verified the following paths:")
     for path in verifiedPaths.values():
         print(path)
@@ -121,6 +122,40 @@ def main():
     except AttributeError as ae:
         logger.error("Unable to find dataframe", exc_info=True)
 
+def main():
+
+    # Refer to directory structure in README.txt
+    # workingDir will be located in the root directory, then the *_Files directories will be located in the working dir
+    # leave testRootDir as is for this to work (will be changed later)
+    rootDir = os.getcwd()
+
+    userPath = get_path_from_user(defaultPath=rootDir, defaultFolder="exeTesting")
+    userFolder = os.path.basename(userPath)
+    exportedFilesFolder = "Exported_Files"
+    resultFilesFolder = "Result_Files"
+
+    # Preparing working dir and testing for relevant files
+    exeDirPath = os.path.dirname(os.path.realpath(__file__))
+
+    # Returns a Dictionary of verified paths
+    verifiedPaths = prepare_working_dir(workingDir=userPath)
+
+    # Get relevant path from the dictionary of verified paths
+    workingDir = verifiedPaths[userFolder]
+    makeLogFile(workingDir)
+    excelPath, l5xFiles = check_working_files(workingDir, logger)
+    exportedFilesPath = verifiedPaths[exportedFilesFolder] 
+    resultFilesPath = verifiedPaths[resultFilesFolder]
+
+    print("\nThe Program has created or verified the following paths:")
+    for path in verifiedPaths.values():
+        print(path)
+    print("File Operations Completed\n--------------------\n")
+    # Create the dicionary object (dataframe) from the excel sheet
+    #dataframe = open_excel_as_pd(excelPath)
+    combo("24-071-Configuration_Routine_RLL.L5X")
+
+    
 if __name__ == "__main__":
     logger = logging.getLogger("main.log")
-    combo("24-071-Configuration_Routine_RLL.L5X")
+    main()
